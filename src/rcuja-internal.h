d47a87e5357473fc6edca55e9f4ec0d608a505d5
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2025-09-26 16:47:27 -0400

    Update Copyright dates
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Change-Id: Ib94c4a4a2b648129865020e48b8a6428c56ee93d

--- src/rcuja-internal.h
+++ src/rcuja-internal.h
@@ -1,3 +1,8 @@
+// SPDX-FileCopyrightText: 2000-2002 Hewlett-Packard Company
+// SPDX-FileCopyrightText: 2012-2025 Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
+//
+// SPDX-License-Identifier: LGPL-2.1-only
+
 #ifndef _URCU_RCUJA_INTERNAL_H
 #define _URCU_RCUJA_INTERNAL_H
 
@@ -5,23 +10,6 @@
  * rcuja/rcuja-internal.h
  *
  * Userspace RCU library - RCU Judy Array Internal Header
- *
- * Copyright (C) 2000 - 2002 Hewlett-Packard Company
- * Copyright 2012-2013 - Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
 #include <pthread.h>
