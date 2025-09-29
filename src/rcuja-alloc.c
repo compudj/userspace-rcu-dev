501f3dde31d79cb0801d03ee9bc0f599524e3de9
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2025-09-29 11:27:55 -0400

    rcuja: Replace shadow node hash table by strided allocator
    The strided allocator places read-side data and metadata on different
    cachelines without wasting space (no padding), without the overhead and
    complexity of a hash table.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Change-Id: Ib27c2f0e23b0298b6f47dd2f90704386852cc7ea

---
+++ src/rcuja-alloc.c
@@ -0,0 +1,291 @@
+// SPDX-FileCopyrightText: 2025 Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
+//
+// SPDX-License-Identifier: LGPL-2.1-only
+
+/*
+ * RCU Judy Array allocator for internal nodes.
+ *
+ * This allocator maximizes cache locality for read-side fast paths
+ * (lookups and traversals) by placing item data and metadata on
+ * different cache lines.
+ *
+ * This is achieved using a strided allocation approach with the
+ * following layout:
+ *
+ * Layout of allocation arena ranges for read-side items of size
+ * item_len (power of 2):
+ *
+ *   nr_items = page_size / item_len.
+ *
+ *   Offset                 Content
+ *
+ *   0:                     array of nr_items elements of item_len each
+ *   page_size:             struct cds_ja_alloc_range
+ *   page_size + sizeof(struct cds_ja_alloc_range):
+ *                          array of nr_items struct cds_ja_metadata_alloc
+ *
+ * An allocation arena contains a linked list of allocation ranges.
+ */
+
+#include <errno.h>
+#include <string.h>
+#include <urcu/rcuja.h>
+#include <urcu/list.h>
+#include "rcuja-internal.h"
+#include "urcu-utils.h"
+
+struct cds_ja_metadata_alloc;
+struct cds_ja_metadata;
+struct cds_ja_alloc_range;
+struct cds_ja_alloc_arena;
+
+static size_t page_size;
+
+struct cds_ja_alloc_range {
+	struct cds_list_head node;		/* Linked list of ranges. */
+	struct cds_ja_alloc_arena *arena;	/* Backward reference to arena. */
+	size_t next_unused;
+
+	struct cds_ja_metadata_alloc metadata[];
+};
+
+struct cds_ja_alloc_arena {
+	struct cds_ja *ja;
+	struct cds_list_head ranges;			/* List head of struct cds_ja_alloc_range. */
+	size_t item_len_order;
+	size_t max_nr_items_per_range;
+	struct cds_ja_metadata_alloc *free_list_head;	/* NULL terminated singly-linked list. */
+	pthread_mutex_t lock;
+	char *name;
+};
+
+static
+struct cds_ja_metadata *cds_ja_range_get_nth_metadata(struct cds_ja_alloc_range *range, size_t n)
+{
+	return &range->metadata[n].metadata;
+}
+
+static
+void *cds_ja_range_get_nth_item(struct cds_ja_alloc_range *range, size_t n)
+{
+	return (((char *) range) - page_size) + (n << range->arena->item_len_order);
+}
+
+static
+struct cds_ja_alloc_range *cds_ja_item_to_range(void *p)
+{
+	void *base = (void *)((unsigned long)p & ~(page_size - 1));
+
+	return (struct cds_ja_alloc_range *) (base + page_size);
+}
+
+static
+struct cds_ja_alloc_range *cds_ja_metadata_to_range(struct cds_ja_metadata *metadata)
+{
+	struct cds_ja_metadata_alloc *metadata_alloc =
+		caa_container_of(metadata, struct cds_ja_metadata_alloc, metadata);
+	size_t index = metadata_alloc->alloc_index;
+
+	return (struct cds_ja_alloc_range *)((char *)(metadata_alloc - index) - sizeof(struct cds_ja_alloc_range));
+}
+
+struct cds_ja_metadata *cds_ja_item_to_metadata(void *p)
+{
+	struct cds_ja_alloc_range *range = cds_ja_item_to_range(p);
+	size_t page_offset = (unsigned long) p & (page_size - 1);
+	size_t index = page_offset >> range->arena->item_len_order;
+
+	return cds_ja_range_get_nth_metadata(range, index);
+}
+
+void *cds_ja_metadata_to_item(struct cds_ja_metadata *metadata)
+{
+	struct cds_ja_metadata_alloc *metadata_alloc =
+		caa_container_of(metadata, struct cds_ja_metadata_alloc, metadata);
+	size_t index = metadata_alloc->alloc_index;
+	struct cds_ja_alloc_range *range = cds_ja_metadata_to_range(metadata);
+
+	return cds_ja_range_get_nth_item(range, index);
+}
+
+static
+size_t cds_ja_arena_range_alloc_size(size_t item_len_order)
+{
+	return page_size + sizeof(struct cds_ja_alloc_range) +
+		(page_size >> item_len_order) * sizeof(struct cds_ja_metadata_alloc);
+}
+
+static
+struct cds_ja_alloc_range *range_create(struct cds_ja_alloc_arena *arena)
+{
+	size_t alloc_size = cds_ja_arena_range_alloc_size(arena->item_len_order);
+	void *ptr = aligned_alloc(page_size, alloc_size);
+	struct cds_ja_alloc_range *range;
+
+	memset(ptr, 0, alloc_size);
+	range = (struct cds_ja_alloc_range *) (ptr + page_size);
+	range->arena = arena;
+	return range;
+}
+
+static
+void range_destroy(struct cds_ja_alloc_range *range)
+{
+	void *p = (void *) range - page_size;
+
+	cds_list_del(&range->node);
+	free(p);
+}
+
+static
+struct cds_ja_alloc_arena *cds_ja_arena_create(struct cds_ja *ja, const char *arena_name, size_t item_len_order)
+{
+	struct cds_ja_alloc_arena *arena = calloc(1, sizeof(struct cds_ja_alloc_arena));
+
+	if (!page_size)
+		page_size = urcu_get_page_len();
+
+	/* item_len must be no larger than page_size. */
+	if ((1UL << item_len_order) > page_size) {
+		errno = EINVAL;
+		return NULL;
+	}
+	if (!arena)
+		return NULL;
+	arena->ja = ja;
+	arena->item_len_order = item_len_order;
+	arena->max_nr_items_per_range = page_size >> item_len_order;
+	CDS_INIT_LIST_HEAD(&arena->ranges);
+	if (arena_name) {
+		arena->name = strdup(arena_name);
+		if (!arena_name)
+			goto error_alloc;
+	}
+	pthread_mutex_init(&arena->lock, NULL);
+	return arena;
+
+error_alloc:
+	free(arena);
+	errno = ENOMEM;
+	return NULL;
+}
+
+static
+void cds_ja_arena_destroy(struct cds_ja_alloc_arena *arena)
+{
+	struct cds_ja_alloc_range *range, *tmp;
+
+	if (!arena)
+		return;
+	pthread_mutex_destroy(&arena->lock);
+	cds_list_for_each_entry_safe(range, tmp, &arena->ranges, node)
+		range_destroy(range);
+	free(arena->name);
+	free(arena);
+}
+
+static
+struct cds_ja_metadata *cds_ja_arena_alloc(struct cds_ja_alloc_arena *arena)
+{
+	struct cds_ja_metadata_alloc *free_list_head, *item;
+	struct cds_ja_alloc_range *range;
+	size_t item_index;
+	void *p;
+
+	pthread_mutex_lock(&arena->lock);
+	free_list_head = arena->free_list_head;
+
+	/* Return head of free list. */
+	if (free_list_head) {
+		arena->free_list_head = free_list_head->free_list_next;
+		p = cds_ja_metadata_to_item(&free_list_head->metadata);
+		memset(p, 0, 1U << arena->item_len_order);
+		memset(&free_list_head->metadata, 0, sizeof(free_list_head->metadata));
+		pthread_mutex_unlock(&arena->lock);
+		return &free_list_head->metadata;
+	}
+	/*
+	 * If there are no ranges, or if the most recent range (first in
+	 * list) does not have any room left, create a new range and
+	 * prepend it to the list head.
+	 */
+	if (cds_list_empty(&arena->ranges))
+		goto create_range;
+	range = cds_list_first_entry(&arena->ranges, struct cds_ja_alloc_range, node);
+	if (range->next_unused + 1 > arena->max_nr_items_per_range)
+		goto create_range;
+	else
+		goto room_left;
+create_range:
+	range = range_create(arena);
+	if (!range) {
+		errno = ENOMEM;
+		pthread_mutex_unlock(&arena->lock);
+		return NULL;
+	}
+	/* Add range to head of list. */
+	cds_list_add(&range->node, &arena->ranges);
+room_left:
+	/* First range in list has room left. */
+	item_index = range->next_unused++;
+	item = &range->metadata[item_index];
+	item->alloc_index = item_index;
+	pthread_mutex_unlock(&arena->lock);
+	return &item->metadata;
+}
+
+struct cds_ja_metadata *cds_ja_alloc_item(struct cds_ja *ja, size_t item_len_order)
+{
+	struct cds_ja_alloc_arena **arena_p;
+
+	if (!page_size)
+		page_size = urcu_get_page_len();
+	if ((1UL << item_len_order) > page_size) {
+		errno = EINVAL;
+		return NULL;
+	}
+	arena_p = &ja->arena_order[item_len_order];
+	if (!*arena_p) {
+		*arena_p = cds_ja_arena_create(ja, "cds_ja_alloc", item_len_order);
+		if (!*arena_p)
+			return NULL;
+	}
+	return cds_ja_arena_alloc(*arena_p);
+}
+
+static
+void cds_ja_free_item_rcu(struct rcu_head *rcu_head)
+{
+	struct cds_ja_metadata_alloc *metadata_alloc =
+		caa_container_of(rcu_head, struct cds_ja_metadata_alloc, rcu_head);
+	struct cds_ja_alloc_arena *arena =
+		cds_ja_metadata_to_range(&metadata_alloc->metadata)->arena;
+
+	pthread_mutex_lock(&arena->lock);
+	metadata_alloc->free_list_next = arena->free_list_head;
+	arena->free_list_head = metadata_alloc;
+	pthread_mutex_unlock(&arena->lock);
+}
+
+void cds_ja_free_item(struct cds_ja_metadata *metadata)
+{
+	struct cds_ja_metadata_alloc *metadata_alloc =
+		caa_container_of(metadata, struct cds_ja_metadata_alloc, metadata);
+	struct cds_ja_alloc_range *range = cds_ja_metadata_to_range(&metadata_alloc->metadata);
+	struct cds_ja_alloc_arena *arena = range->arena;
+	const struct rcu_flavor_struct *flavor = arena->ja->flavor;
+
+	flavor->update_call_rcu(&metadata_alloc->rcu_head, cds_ja_free_item_rcu);
+}
+
+void cds_ja_free_all_arenas(struct cds_ja *ja)
+{
+	int i;
+
+	for (i = 0; i <= RCU_JA_ALLOC_ORDER_MAX; i++) {
+		if (!ja->arena_order[i])
+			continue;
+		cds_ja_arena_destroy(ja->arena_order[i]);
+		ja->arena_order[i] = NULL;
+	}
+}
--- src/urcu-utils.h
+++ src/urcu-utils.h
@@ -10,6 +10,9 @@
  */
 
 #include <urcu/config.h>
+#include <unistd.h>
+
+#define URCU_DEFAULT_PAGE_SIZE	4096
 
 #define urcu_stringify(a) _urcu_stringify(a)
 #define _urcu_stringify(a) #a
@@ -27,5 +30,112 @@
 		type __min2 = (y);              	\
 		__min1 <= __min2 ? __min1: __min2;	\
 	})
+
+#define __urcu_align_mask(v, mask)	(((v) + (mask)) & ~(mask))
+#define urcu_align(v, align)		__urcu_align_mask(v, (__typeof__(v)) (align) - 1)
+
+static inline
+unsigned int urcu_fls_u64(uint64_t x)
+{
+	unsigned int r = 64;
+
+	if (!x)
+		return 0;
+
+	if (!(x & 0xFFFFFFFF00000000ULL)) {
+		x <<= 32;
+		r -= 32;
+	}
+	if (!(x & 0xFFFF000000000000ULL)) {
+		x <<= 16;
+		r -= 16;
+	}
+	if (!(x & 0xFF00000000000000ULL)) {
+		x <<= 8;
+		r -= 8;
+	}
+	if (!(x & 0xF000000000000000ULL)) {
+		x <<= 4;
+		r -= 4;
+	}
+	if (!(x & 0xC000000000000000ULL)) {
+		x <<= 2;
+		r -= 2;
+	}
+	if (!(x & 0x8000000000000000ULL)) {
+		x <<= 1;
+		r -= 1;
+	}
+	return r;
+}
+
+static inline
+unsigned int urcu_fls_u32(uint32_t x)
+{
+	unsigned int r = 32;
+
+	if (!x)
+		return 0;
+	if (!(x & 0xFFFF0000U)) {
+		x <<= 16;
+		r -= 16;
+	}
+	if (!(x & 0xFF000000U)) {
+		x <<= 8;
+		r -= 8;
+	}
+	if (!(x & 0xF0000000U)) {
+		x <<= 4;
+		r -= 4;
+	}
+	if (!(x & 0xC0000000U)) {
+		x <<= 2;
+		r -= 2;
+	}
+	if (!(x & 0x80000000U)) {
+		x <<= 1;
+		r -= 1;
+	}
+	return r;
+}
+
+static inline
+unsigned int urcu_fls_ulong(unsigned long x)
+{
+#if CAA_BITS_PER_LONG == 32
+	return urcu_fls_u32(x);
+#else
+	return urcu_fls_u64(x);
+#endif
+}
+
+/*
+ * Return the minimum order for which x <= (1UL << order).
+ * Return -1 if x is 0.
+ */
+static inline
+int urcu_get_count_order_ulong(unsigned long x)
+{
+	if (!x)
+		return -1;
+
+	return urcu_fls_ulong(x - 1);
+}
+
+static inline
+unsigned long urcu_get_page_len(void)
+{
+	long page_len = sysconf(_SC_PAGE_SIZE);
+
+	if (page_len < 0)
+		page_len = URCU_DEFAULT_PAGE_SIZE;
+	return (unsigned long) page_len;
+}
+
+static inline
+bool urcu_is_pow2(uint64_t x)
+{
+	return !(x & (x - 1));
+}
 
 #endif /* _URCU_UTILS_H */
