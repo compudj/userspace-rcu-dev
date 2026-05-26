1fbd2155567c86b980670b359167710b79071fbb
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2026-05-26 15:08:21 -0400

    Fractal Trie: disable DEBUG_CLEAR_ITER by default
    DEBUG_CLEAR_ITER was #defined unconditionally while its siblings DEBUG
    and DEBUG_COUNTERS are commented out, so every cds_ft_iter_reset()
    memset the full path and key buffers (max_tree_depth pointers +
    max_key_len bytes) in production builds.  Comment it out to match the
    other debug switches; uncomment to re-enable when debugging.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>

--- src/fractal-trie-internal.h
+++ src/fractal-trie-internal.h
@@ -1113,7 +1113,7 @@ bool cds_ft_metadata_in_recompact_private(struct cds_ft_metadata *metadata);
 
 //#define DEBUG
 //#define DEBUG_COUNTERS
-#define DEBUG_CLEAR_ITER
+//#define DEBUG_CLEAR_ITER
 
 #ifdef __linux__
 #include <syscall.h>
